01440ead81bbcc0947ae8b31e7b38f73325adbcb
suyeon-dev <choilynne.dev@gmail.com>
2025-04-16 10:47:18 +0900

    feat(header):내비게이션 구조 변경 및 메뉴명 수정 - 1차 MVP 배포를 위한 핵심 기능 위주 메뉴 항목 재정비 - '대시보드', '커뮤니티' 메뉴 항목 숨김 처리 - '수영일기' 하위 메뉴 중 '캘린더', '컬렉션' 항목 비활성화 - issue #2

--- src/components/common/Navigation.tsx
+++ src/components/common/Navigation.tsx
@@ -1,30 +1,30 @@
 'use client';
 
-import { useDropdownStore } from '@/store/useDropdownStore';
+// import { useDropdownStore } from '@/store/useDropdownStore';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 // 내비게이션 항목 타입
 const NAV_ITEMS = [
-  { name: '대시보드', href: '/dashboard' },
-  { name: '수영일기', href: '/diary', hasDropdown: true },
+  { name: '소개', href: '/' },
+  { name: '일기쓰기', href: '/diary/write' },
+  { name: '아카이브', href: '/diary/archive' },
   { name: '지도', href: '/map' },
-  { name: '커뮤니티', href: '/community' },
 ];
 
 export default function Navigation() {
   const pathname = usePathname(); // 현재 경로 가져오기
-  const { toggle } = useDropdownStore();
+  // const { toggle } = useDropdownStore();
 
   return (
     <nav className='flex gap-8 font-bold text-lg'>
-      {NAV_ITEMS.map(({ name, href, hasDropdown }) => (
+      {NAV_ITEMS.map(({ name, href }) => (
         <Link
           key={href}
           href={href}
-          onClick={() => {
-            if (hasDropdown) toggle();
-          }}
+          // onClick={() => {
+          //   if (hasDropdown) toggle();
+          // }}
           className={`nav-link ${pathname === href ? 'text-blue-500' : ''}`}
         >
           {name}
