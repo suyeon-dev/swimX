6194f16139ac8bef2d16944a22ec713ac1c7fe9d
suyeon-dev <choilynne.dev@gmail.com>
2025-04-20 14:42:28 +0900

    feat(home):메인 페이지 로그인 상태에 따른 버튼 분기 및 소개 섹션 추가 - 로그인 여부에 따라 HeroSection 버튼 분기 처리 - 일기쓰기, 아카이빙 기능 소개 컴포넌트 추가 및 반응형 적용 - 전체 레이아웃 및 스타일 개선 - issue #15

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
-'use client';
-
 import Footer from '@/components/common/Footer';
 import HeroSection from '@/components/home/HeroSection';
-import ServiceIntroSection from '@/components/home/ServiceIntroSection';
-import { useSession } from 'next-auth/react';
+import DiaryIntroSection from '@/components/home/DiaryIntroSection';
+import ArchiveIntroSection from '@/components/home/ArchiveIntroSection';
 
 export default function Home() {
-  const { data: session, status } = useSession(); // 현재 로그인 세션 정보 조회
-  console.log('세션 정보: ', session);
-  console.log('세션 상태: ', status);
   return (
     <>
       <HeroSection />
-      <ServiceIntroSection />
+      <DiaryIntroSection />
+      <ArchiveIntroSection />
       <Footer />
     </>
   );
